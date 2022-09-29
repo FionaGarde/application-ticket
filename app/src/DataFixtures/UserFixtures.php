2888f8c6cd34f169ac85baa4d3c73d89239b6add
root <root@endrick.localdomain>
2022-09-29 12:33:46 +0200

    feat(faker) add faker for fake user name

--- app/src/DataFixtures/UserFixtures.php
+++ app/src/DataFixtures/UserFixtures.php
@@ -2,17 +2,19 @@
 
 namespace App\DataFixtures;
 
+use Faker\Factory;
 use App\Entity\User;
-use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class UserFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
         for ($i = 0; $i < 20; $i++) {
+            $faker = Factory::create('FR,fr');
             $user = new User();
-            $user->setEmail('user'.$i.'@my-digital-school.org');
+            $user->setEmail($faker->firstName().".".$faker->lastName().'@my-digital-school.org');
             $user->setPassword('userpass');
             $manager->persist($user);
         }
