3662a5787424901d2c56eb482db7c9fa845a72c6
jimmy-martin <jimmy.martin952@gmail.com>
2022-09-30 12:31:12 +0200

    add admin redirection on login

--- app/src/Controller/HomeController.php
+++ app/src/Controller/HomeController.php
@@ -12,6 +12,11 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(UserRepository $userRepository): Response
     {
+        $connectedUserRoles = $this->getUser()->getRoles();
+        if (in_array('ROLE_ADMIN', $connectedUserRoles)) {
+            return $this->redirectToRoute('admin_home');
+        }
+
         $users = $userRepository->findAllWithoutSpecifiedId($this->getUser()->getId());
 
         return $this->render('home/index.html.twig', [
