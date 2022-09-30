98d1ed96ea0abe4468a27c0ea0cc9ba82cc513a5
jimmy-martin <jimmy.martin952@gmail.com>
2022-09-30 12:14:58 +0200

    add preselection of user when clicking on new message on student selection page

--- app/src/Controller/UserController.php
+++ app/src/Controller/UserController.php
@@ -18,9 +18,8 @@ class UserController extends AbstractController
         // TODO: préparer la route et les données à afficher sur la page d'accueil de l'admin
         // TODO: préparer la route et les données à afficher pour les stats de l'admin
         // TODO: préparer la route pour la page qui va lister tous les message à modérer pour l'admin
-        // TODO: page qui liste les élèves de la plateforme sauf l'élève connecté pour qu'ils puissent envoyer un message
-        // TODO: permettre de présélectionner un user depuis la liste des élèves
         // TODO: afficher sur la home le message s'il a déjà envoyé un message et s'il a été validé ou pas, (si non validé possibilité de renvoyer vers la page de modification de message
+        // TODO: Faire en sorte de redirigier vers l'accueil ADMIN si "ROLE_ADMIN"
     }
 
     #[Route('/', name: 'user_index')]
